74afa287a5304dfdd6f74de6bb3446ffaeac51cf
pengsanchun <pengsanchun@58.com>
2019-12-16 13:52:42 +0800

    M

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/pscblog/'
+    ? '/pscblog/dist/'
     : '/', // 基本路径
   outputDir: 'dist', // 输出文件目录
   lintOnSave: false, // eslint-loader 是否在保存的时候检查
